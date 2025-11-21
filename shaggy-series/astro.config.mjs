0eb3b37edcda15e46e51c8fa7597db0f60ac97d9
larswurm <lars.wurm1@inter.ikea.com>
2025-11-21 12:48:08 +0100

    fix: add .nojekyll file to allow GitHub Pages to serve _astro folder

--- shaggy-series/astro.config.mjs
+++ shaggy-series/astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://zekt-dev-org.github.io/zekt-docs/',
-	base: '/',
+	base: '/zekt-docs/',
 	integrations: [
 		starlight({
 			title: 'Zekt Docs',
